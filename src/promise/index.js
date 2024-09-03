c60eb47890a8199e715cb5cdcf298a2da052d192
Lauramgr22 <lauramgr6@gmail.com>
2024-09-03 08:37:15 -0500

    new promise

---
+++ src/promise/index.js
@@ -0,0 +1,18 @@
+const promise = new Promise(function(resolve, reject){
+    resolve('hey!')
+})
+
+const cows = 16
+const countCows = new Promise(function(resolve, reject){
+    if(cows > 10){
+        resolve(`we have ${cows} cows on the farm` )
+    } else{
+        reject("there is no cows on the farm")
+    }
+})
+
+countCows.then((result)=>{
+    console.log(result)
+}).catch((error)=>{
+    console.log(error)
+}).finally(()=>console.log("finally"))
